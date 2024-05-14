22d12ff7a919d4890e35370212d1eb60de4cd50b
nachool <nachma.olivia1394@gmail.com>
2024-05-14 18:36:36 +0700

    memanggil destruktor

--- Destruktorr/Destruktorr.cpp
+++ Destruktorr/Destruktorr.cpp
@@ -54,6 +54,11 @@ void angka::isiData()
 
 int main()
 {
-    
-}
+    angka belajarcpp(3);                    // Constructor Dipanggil
+    angka* ptrBelajarcpp = new angka(5);    // Constructor Dipanggil
+    delete ptrBelajarcpp;                   // Destructor Dipanggil
+
+    return 0;
+
+} //memanggil destruktor
 

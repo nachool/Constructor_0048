fe8f150dd05e26c3237b8237c2ef126f9c6962b5
nachool <nachma.olivia1394@gmail.com>
2024-05-14 18:34:57 +0700

    mnambah class angka

---
+++ Destruktorr/Destruktorr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class angka
+{
+private:
+    int* arr;
+    int panjang;
+
+public:
+    angka(int);     // Constructor
+    ~angka();       // Destructor
+    void cetakData();
+    void isiData();
+};
+
+
+
+int main()
+{
+    
+}
+

c3f4e9b2042e182c5876a9d5d51d24d81d91180b
nachool <nachma.olivia1394@gmail.com>
2024-05-14 17:31:54 +0700

    menambah library string

--- mbrStatik/mbrStatik.cpp
+++ mbrStatik/mbrStatik.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+
+	void printAll();
+	mahasiswa(string pnama) {
+		nama = pnama;
+		setID();
+	}
+};
+
+int mahasiswa::nim = 113;
+
 
 int main()
 {

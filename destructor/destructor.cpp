e500bc27078241071d4d2771bbf68db6862368da
nachool <nachma.olivia1394@gmail.com>
2024-05-14 18:29:09 +0700

    membrr function

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -10,8 +10,17 @@ public:
     ~angka();       // Destructor
     void cetakData();
     void isiData();
-
+        
 };
+
+// Definisi member Function
+angka::angka(int i)  // Constructor
+{
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
 
 int main()
 {
